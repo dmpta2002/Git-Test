60df98c0c8b5a034a0c34268efc6a7fe901b5ba4
Daniel Moore <dmpta2002@yahoo.com>
2024-05-27 20:46:00 -0500

    Updated code with static streamlit

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-# Hello World
-print ("Hello World")
+import streamlit as st
+
+def main():
+    st.title("Hello, World!")
+    st.write("This is a Streamlit app that displays a simple 'Hello, World!' message.")
+
+if __name__ == "__main__":
+    main()
